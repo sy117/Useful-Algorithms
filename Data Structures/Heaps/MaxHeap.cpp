a4f430b94552e6b150245c351161c967e6f8763c
sy117 <07sunilraj@gmail.com>
2024-08-25 23:58:09 +0530

    updated Heap DS

--- Data Structures/Heaps/MaxHeap.cpp
+++ Data Structures/Heaps/MaxHeap.cpp
@@ -6,51 +6,43 @@ using namespace std;
 const int N = 100005;
 int heap_size = N;
 
-void swap(int *a, int *b)
-{
+void swap(int *a, int *b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-void max_heapify(int a[], int i)
-{
-	int l,r,largest;
+void max_heapify(int a[], int i) {
+	int l, r, largest;
 	l = 2*i+1;
 	r = 2*i+2;
-	if(l < heap_size && a[l] > a[i])
-	{
+	if(l < heap_size && a[l] > a[i]) {
 		largest = l;
 	}
-	else
-	{
+	else {
 		largest = i;
 	}
 	
-	if(r < heap_size && a[r] > a[largest])
-	{
+	if(r < heap_size && a[r] > a[largest]) {
 		largest = r;
 	}
 	
-	if(largest != i)
-	{
+	if(largest != i) {
 		swap(&a[i], &a[largest]);
 		max_heapify(a, largest);
 	}
 }
 
-void build_max_heap(int a[], int n)
-{
+void build_max_heap(int a[], int n) {
 	heap_size = n;
 	int i = ceil(n/2);
-	for(;i>=0;i--)
+	for(; i>=0; i--)
 	{
 		max_heapify(a, i);
 	}
 }
 
-int extract_max(int a[])
-{
+int extract_max(int a[]) {
 	if(heap_size==0)
 	{
 		return -1;
@@ -61,20 +53,17 @@ int extract_max(int a[])
 	max_heapify(a, 0);
 	return max_element;
 }
-int main()
-{
+int main() {
 	int n;
 	cin>>n;
 	int arr[n];
-	for(int i=0;i<n;i++)
-	{
-		cin>>arr[i];
+	for(int i=0; i<n; i++) {
+		cin >> arr[i];
 	}
-	build_max_heap(arr,n);
-	for(int i=0;i<n;i++)
-	{
+	build_max_heap(arr, n);
+	for(int i=0; i<n; i++) {
 		int k = extract_max(arr);
-		cout<<k<<" \n"[i==n-1];
+		cout << k << " \n"[i==n-1];
 	}
 	return 0;
 }
