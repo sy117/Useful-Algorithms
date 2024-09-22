b8bca5f19bf44492ab77976894e345e649b949a4
sy117 <07sunilraj@gmail.com>
2024-09-22 16:19:25 +0530

    Topological Sorting

---
+++ Graph Algorithms/TopologicalSortingBFS.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*
+	TopologicalSorting:- Linear ordering of vertices such that of there is an edge
+    form u to v then, u appears before v in ordering
+
+    NOTE:- Applicable only for DAG(Directed Acyclic Graph)
+
+    Kahn's Algorithm:- Uses BFS + Indegree concept to find topological sorting
+	
+*/
+ 
+
+vector<int> topoSort(int n, vector<int> adj[]) {
+    vector<int> inDegree(n, 0);
+    for(int u=0; u<n; u++) {
+        for(auto v: adj[u]) {
+            inDegree[v]++;
+        }
+    }
+    queue<int> qu;
+    for(int i=0; i<n; i++) {
+        if(inDegree[i] == 0) {
+            qu.push(i);
+        }
+    }
+    vector<int> res;
+    while(!qu.empty()) {
+        int node = qu.front();
+        qu.pop();
+        res.push_back(node);
+        for(auto ch: adj[node]) {
+            inDegree[ch]--;
+            if(inDegree[ch] == 0) {
+                qu.push(ch);
+            }
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n];
+    int u, v;
+    for(int i=0; i<m; i++) {
+        cin >> u >> v;
+        u--; v--; // 0-based indexing
+        adj[u].push_back(v);
+    }
+
+    vector<int> topo = topoSort(n, adj);
+
+    for(int v: topo) {
+    	cout << v << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+/*
+    Example TC:-
+    6 6
+    6 1
+    5 2
+    6 3
+    3 4
+    4 2
+    5 2
+
+    Output:- [4 5 0 2 3 1]
+*/
---
+++ Graph Algorithms/TopologicalSortingDFS.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*
+	TopologicalSorting:- Linear ordering of vertices such that of there is an edge
+    form u to v then, u appears before v in ordering
+
+    NOTE:- Applicable only for DAG(Directed Acyclic Graph)
+
+    Approach:- Why DFS works in this case is because if there is a directed edge 
+    from u to v (u -> v) and dfs call to v goes through u then dfs(v) will be
+    completed first and then dfs(u)
+	
+*/
+ 
+
+void dfs(int node, vector<bool>& vis, vector<int>& topo, vector<int> adj[]) {
+    vis[node] = true;
+
+    for(int neighbour: adj[node]) {
+        if(!vis[neighbour]) {
+           dfs(neighbour, vis, topo, adj);
+        }
+    }
+    topo.push_back(node);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n];
+    int u, v;
+    for(int i=0; i<m; i++) {
+        cin >> u >> v;
+        u--; v--; // 0-based indexing
+        adj[u].push_back(v);
+    }
+
+    vector<bool> vis(n, false);
+    vector<int> topo;
+    // handling disconnected components
+    for(int i=0; i<n; i++) {
+    	if(!vis[i]) {
+            dfs(i, vis, topo, adj);
+        }
+    }
+    reverse(topo.begin(), topo.end());
+
+    for(int v: topo) {
+    	cout << v << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
